e257d0960f4ad488c6c4eb0c738afce07cd56a89
Yuri Kulikov <intysite@gmail.com>
2022-08-22 18:31:48 +0300

    Задача 2

--- src/lesson2/HomeWork1.java
+++ src/lesson2/HomeWork1.java
@@ -1,6 +1,9 @@
 package lesson2;
 
 import lesson2.task1.*;
+import lesson2.task2.MyList;
+
+import java.util.List;
 
 public class HomeWork1 {
     public static void main(String[] args) {
@@ -34,9 +37,13 @@ public class HomeWork1 {
         //Задача №2
         //Создать класс MyList с атрибутами T[] objects, int size. T - generics.
         //Массив T[] будет создаваться при создании экземпляра класса MyList с размерностью 3.
-        //Создать метод add. Он будет добавлять элемент в массив object. Если в массиве уже 3 элемента есть, то нужно:
+        //Создать метод add. Он будет добавлять элемент в массив objects. Если в массиве уже 3 элемента есть, то нужно:
         //1.Создать новый массив T[] с размерностью в два раза больше предыдущей.
         //2.Скопировать со старого массива все значения в новый
         //3.Присвоить экземпляру MyList новый массив.
+
+        MyList<String> objectMyList = new MyList<>();
+        objectMyList.add("O kak");
+        System.out.println(objectMyList);
     }
 }
---
+++ src/lesson2/task2/MyList.java
@@ -0,0 +1,33 @@
+package lesson2.task2;
+
+import java.util.Arrays;
+
+public class MyList<T> {
+    private T[] objects = (T[]) new Object[3];
+    private int size = 0;
+
+    public boolean add(T object) {
+        if(size == objects.length) {
+            T[] newArray = (T[]) new Object[objects.length * 2];
+            for (int i = 0; i < objects.length; i++) {
+                newArray[i] = objects[i];
+            }
+            this.objects = newArray;
+        }
+        for (int i = 0; i < objects.length; i++) {
+            if(objects[i] == null) {
+                objects[i] = object;
+                break;
+            }
+        }
+        size++;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MyList{" +
+                "objects=" + Arrays.toString(objects) +
+                '}';
+    }
+}
