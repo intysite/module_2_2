3bc8c59685fdbdf0d6c93599f72ed3dec64d7797
Yuri Kulikov <intysite@gmail.com>
2022-09-04 14:46:24 +0300

    Домашняя работа 3

---
+++ src/lesson6/User.java
@@ -0,0 +1,12 @@
+package lesson6;
+
+public class User {
+    private int age;
+    private String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+}
