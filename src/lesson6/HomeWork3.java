888879df1b42d82c1008c89bf24e283d3887ca02
Yuri Kulikov <intysite@gmail.com>
2022-09-05 16:38:03 +0300

    Домашняя работа 3 задача 1

--- src/lesson6/HomeWork3.java
+++ src/lesson6/HomeWork3.java
@@ -1,4 +1,43 @@
 package lesson6;
 
+import java.util.HashSet;
+
 public class HomeWork3 {
+    public static void main(String[] args) {
+
+        //Задача №1
+        //1. Создать класс User, с двумя атрибутами - int age, String name
+        //2. Создать экземпляр класса HashSet<User>
+        //3. Переопределить hashcode и equals у User так, чтобы при записи в HashSet, были только уникальные name юзера
+
+        User vasya = new User(20, "Vasya");
+        User vanya = new User(25, "Vanya");
+        User petya = new User(25, "Petya");
+        User vanya1 = new User(25, "Vanya");
+
+        HashSet<User> hashUser = new HashSet<User>();
+        hashUser.add(vasya);
+        hashUser.add(vanya);
+        hashUser.add(petya);
+        hashUser.add(vanya1);
+        for (User user : hashUser) {
+            System.out.println(user);
+        }
+
+        //Задача №2
+        //1. Создать класс UserBroken, с двумя атрибутами - int age, String name
+        //2. Создать экземпляр класса HashSet<UserBroken>
+        //3. Переопределить hashcode и equals так, чтобы при вставке любого юзера в HashSet,
+        // в hashSet всегда оставался один юзер
+
+
+
+
+        //Задача №3
+        //1. Создать класс CarBroken
+        //2. Создать экземпляра класса HashMap<CarBroken, Integer>
+        //3. Переопределить hashcode и equals так, чтобы при вставке любого CarBroken в HashMap,
+        //всегда CarBroken попадал в один бакет.
+
+    }
 }
--- src/lesson6/User.java
+++ src/lesson6/User.java
@@ -1,5 +1,7 @@
 package lesson6;
 
+import java.util.Objects;
+
 public class User {
     private int age;
     private String name;
@@ -9,4 +11,24 @@ public class User {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
